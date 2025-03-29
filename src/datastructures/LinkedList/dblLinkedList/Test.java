f265443eb8c9fe77b81fb21725d965b3fa9963c0
Breno <joaobaise@hotmail.com>
2025-03-29 12:37:46 +0000

    Added base for doubly linked list

---
+++ src/datastructures/LinkedList/dblLinkedList/Test.java
@@ -0,0 +1,12 @@
+package datastructures.LinkedList.dblLinkedList;
+
+public class Test {
+    public static void main(String[] args) {
+        DoublyLinkedList dll = new DoublyLinkedList(7);
+        dll.getHead();
+        dll.getTail();
+        dll.getLength();
+        dll.printList();
+
+    }
+}
